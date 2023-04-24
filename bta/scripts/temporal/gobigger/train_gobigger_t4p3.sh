c81c68aa05d46c49ffa65fc87cb6852f0687f676
Li Zhiyuan <liz23@login3.triton.aalto.fi>
2023-04-24 17:41:39 +0300

    change train script

--- bta/scripts/temporal/gobigger/train_gobigger_t4p3.sh
+++ bta/scripts/temporal/gobigger/train_gobigger_t4p3.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
+#SBATCH --mem=100G
 #SBATCH --time=72:00:00
 #SBATCH --array=0-1
 
@@ -24,7 +24,7 @@ episode_length=200
 CUDA_VISIBLE_DEVICES=0 singularity exec --bind /scratch --nv /scratch/work/liz23/bpta.sif python ../../train/train_gobigger.py \
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
---save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
+--save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 50 --eval_episodes 100 \
 --representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --ppo_epoch 15 --num_mini_batch 2 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
 --max_edges 10  --time_channels 100 --time_gap 10
--- bta/scripts/temporal/overcooked/train_overcooked_distant_tomato.sh
+++ bta/scripts/temporal/overcooked/train_overcooked_distant_tomato.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
+#SBATCH --mem=100G
 #SBATCH --time=72:00:00
 #SBATCH --array=0-1
 
