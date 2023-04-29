d0648aee2d6d3d19eb9eec95dae9da99df86a2d9
Li Zhiyuan <liz23@login3.triton.aalto.fi>
2023-04-29 16:49:15 +0300

    change script

--- bta/scripts/temporal/football/train_football_corner.sh
+++ bta/scripts/temporal/football/train_football_corner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=football-temporal
-#SBATCH --partition=batch
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64G
@@ -26,6 +27,6 @@ CUDA_VISIBLE_DEVICES=0 singularity exec --bind /scratch --nv /scratch/work/liz23
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
 --representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
---max_grad_norm 0.5 --lr 5e-4 --critic_lr 5e-4 \
+--max_grad_norm 5.0 --lr 5e-4 --critic_lr 5e-4 --entropy_coef 0.05 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
 --max_edges 10  --time_channels 100 --time_gap 10
