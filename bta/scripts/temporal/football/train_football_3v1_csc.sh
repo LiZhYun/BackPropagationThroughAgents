8dce7ba56a2ef4b8c55a9452b33e08ea74d4e913
Zhiyuan Li <lizhiyua@puhti-login14.bullx>
2023-05-09 19:22:33 +0300

    script

--- bta/scripts/temporal/football/train_football_3v1_csc.sh
+++ bta/scripts/temporal/football/train_football_3v1_csc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --account=project_2007776
-#SBATCH --partition=longrun
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
-#SBATCH --time=280:00:00
+#SBATCH --time=24:00:00
 
 #--gres=gpu:v100:1
 export SING_IMAGE=/projappl/project_2007776/bpta.sif
@@ -21,13 +22,14 @@ exp="check"
 num_agents=3
 
 # train param
-num_env_steps=10000000
+num_env_steps=25000000
 episode_length=200
 
 apptainer_wrapper exec python ../../train/train_football.py \
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
---save_interval 200000 --log_interval 200000 --use_eval --eval_interval 200000 --n_eval_rollout_threads 100 --eval_episodes 100 \
+--save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
 --representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --num_mini_batch 2 \
+--max_grad_norm 3.5 --lr 5e-4 --critic_lr 5e-4 --entropy_coef 0.05 --temperature 1.0\
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
 --max_edges 10  --time_channels 100 --time_gap 10
