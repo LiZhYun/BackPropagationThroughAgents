4034c192b5f74a6de49d9fec3c9613ecda539291
Zhiyuan Li <lizhiyua@puhti-login14.bullx>
2023-05-10 12:28:40 +0300

    corner

---
+++ bta/scripts/temporal/football/train_football_corner_csc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --account=project_2007776
+#SBATCH --partition=small
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+
+#--gres=gpu:v100:1
+
+export SING_IMAGE=/projappl/project_2007776/bpta.sif
+export SING_FLAGS=--nv
+export SING_FLAGS="-B /scratch/project_2007776 $SING_FLAGS"
+
+# exp param
+env="Football"
+scenario="academy_corner"
+algo="temporal" # "mappo" "ippo"
+exp="check"
+
+# football param
+num_agents=10
+
+# train param
+num_env_steps=25000000
+episode_length=200
+
+apptainer_wrapper exec python ../../train/train_football.py \
+--env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
+--num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
+--save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
+--representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
+# --max_grad_norm 5.0 --lr 5e-4 --critic_lr 5e-4 --entropy_coef 0.05 \
+--threshold 0.0 \
+--user_name "zhiyuanli" --wandb_name "zhiyuanli" \
+--max_edges 10  --time_channels 100 --time_gap 10
