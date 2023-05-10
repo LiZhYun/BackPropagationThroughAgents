f75330b85db5969159eff2f5ad1cec3dde7adc9d
Zhiyuan Lee <35086644+LiZhYun@users.noreply.github.com>
2023-05-10 20:27:54 +0300

    matrix

--- bta/runner/temporal/matrix_runner.py
+++ bta/runner/temporal/matrix_runner.py
@@ -37,7 +37,8 @@ class MatrixRunner(Runner):
                     self.trainer[agent_id].policy.lr_decay(episode, episodes)
 
             self.temperature = max(self.all_args.temperature - (self.all_args.temperature * (episode / float(episodes))), 1.0)
-            self.agent_order = torch.tensor([i for i in range(self.num_agents)]).unsqueeze(0).repeat(self.n_rollout_threads, 1).to(self.device)
+            self.agent_order = torch.randperm(self.num_agents).unsqueeze(0).repeat(self.n_rollout_threads, 1).to(self.device)
+            # self.agent_order = torch.tensor([i for i in range(self.num_agents)]).unsqueeze(0).repeat(self.n_rollout_threads, 1).to(self.device)
 
             for step in range(self.episode_length):
                 # Sample actions
@@ -310,8 +311,8 @@ class MatrixRunner(Runner):
             execution_masks = adjs.clone().permute(0, 2, 1).contiguous().view(-1, self.num_agents)
         else:
             #test
-            # eval_order = torch.stack([torch.randperm(self.num_agents) for _ in range(self.n_eval_rollout_threads)]).to(self.device)
-            eval_order = torch.tensor([i for i in range(self.num_agents)]).unsqueeze(0).repeat(self.n_eval_rollout_threads, 1).to(self.device)
+            eval_order = torch.stack([torch.randperm(self.num_agents) for _ in range(self.n_eval_rollout_threads)]).to(self.device)
+            # eval_order = torch.tensor([i for i in range(self.num_agents)]).unsqueeze(0).repeat(self.n_eval_rollout_threads, 1).to(self.device)
             ordered_vertices = _t2n(eval_order)
             execution_masks = generate_mask_from_order(
             eval_order, ego_exclusive=False).to(
