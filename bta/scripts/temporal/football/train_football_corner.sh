1e8d644b54c19101b5af5101c821f0da7c2c39e1
Zhiyuan Li <lizhiyua@puhti-login14.bullx>
2023-05-10 12:24:35 +0300

    corner

--- bta/scripts/temporal/football/train_football_corner.sh
+++ bta/scripts/temporal/football/train_football_corner.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=football-temporal
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64G
-#SBATCH --time=72:00:00
-#SBATCH --array=0-1
+#SBATCH --time=24:00:00
+#SBATCH --array=0
 
 # exp param
 env="Football"
@@ -27,6 +26,7 @@ CUDA_VISIBLE_DEVICES=0 singularity exec --bind /scratch --nv /scratch/work/liz23
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
 --representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
---max_grad_norm 5.0 --lr 5e-4 --critic_lr 5e-4 --entropy_coef 0.05 \
+# --max_grad_norm 5.0 --lr 5e-4 --critic_lr 5e-4 --entropy_coef 0.05 \
+--threshold 1.0 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
 --max_edges 10  --time_channels 100 --time_gap 10
