2afb63f0eedb7386ac5db49b6f2051d8cb738d30
Li Zhiyuan <liz23@login3.triton.aalto.fi>
2023-05-12 12:48:45 +0300

    corner

--- bta/scripts/temporal/football/train_football_corner.sh
+++ bta/scripts/temporal/football/train_football_corner.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
-#SBATCH --time=24:00:00
-#SBATCH --array=0
+#SBATCH --mem=32G
+#SBATCH --time=48:00:00
+#SBATCH --array=0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100
 
 # exp param
 env="Football"
@@ -20,13 +20,15 @@ num_agents=10
 # train param
 num_env_steps=25000000
 episode_length=200
+deno=100
+threshold=`echo "scale=2; $SLURM_ARRAY_TASK_ID / $deno" | bc`
+
+echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, threshold is ${threshold}"
 
 CUDA_VISIBLE_DEVICES=0 singularity exec --bind /scratch --nv /scratch/work/liz23/bpta.sif python ../../train/train_football.py \
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
 --representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
-# --max_grad_norm 5.0 --lr 5e-4 --critic_lr 5e-4 --entropy_coef 0.05 \
---threshold 1.0 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
---max_edges 10  --time_channels 100 --time_gap 10
+--max_edges 10  --time_channels 100 --time_gap 10 --threshold ${threshold}
