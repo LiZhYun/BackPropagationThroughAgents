03b195e1a3704cf0d0dc31da16e4f300ccca3b6b
Li Zhiyuan <liz23@login3.triton.aalto.fi>
2023-05-12 17:21:55 +0300

    sh

--- bta/scripts/temporal/gobigger/train_gobigger_t2p2.sh
+++ bta/scripts/temporal/gobigger/train_gobigger_t2p2.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=128G
 #SBATCH --time=72:00:00
-#SBATCH --array=0-1
+#SBATCH --array=0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100
 
 module restore bpta
 source activate pytorch-env
@@ -19,16 +19,17 @@ scenario="t2p2"
 num_agents=2
 algo="temporal"
 exp="check"
+deno=100
+threshold=`echo "scale=2; $SLURM_ARRAY_TASK_ID / $deno" | bc`
 
 # train param
 num_env_steps=10000000
-episode_length=128
+episode_length=200
 
 python ../../train/train_gobigger.py \
 --env_name ${env} --scenario_name ${scenario} --team_num ${team_num} --player_num_per_team ${player_num_per_team} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 100000 --log_interval 100000 --use_eval --eval_interval 200000 --n_eval_rollout_threads 5 --eval_episodes 100 \
 --representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 32 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
---max_grad_norm 0.5 --lr 1e-4 --critic_lr 5e-4 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
---max_edges 10  --time_channels 100 --time_gap 10
+--max_edges 10  --time_channels 100 --time_gap 10 --threshold ${threshold} --entropy_coef 0.05
--- bta/scripts/temporal/overcooked/train_overcooked_distant_tomato.sh
+++ bta/scripts/temporal/overcooked/train_overcooked_distant_tomato.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=10G
 #SBATCH --time=72:00:00
-#SBATCH --array=0-1
+#SBATCH --array=0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100
 
 module restore bpta
 source activate pytorch-env
@@ -24,14 +24,15 @@ fi
 num_agents=2
 algo="temporal"
 exp="check"
+deno=100
+threshold=`echo "scale=2; $SLURM_ARRAY_TASK_ID / $deno" | bc`
 
 echo "env is ${env}, layout is ${layout}, algo is ${algo}, exp is ${exp} version is ${version}"
 
 python ../../train/train_overcooked.py \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --layout_name ${layout} --num_agents ${num_agents} \
     --seed 1 --n_training_threads 1 --n_rollout_threads 100 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --reward_shaping_horizon 100000000 \
-    --ppo_epoch 15 --clip_param 0.2 \
-    --max_grad_norm 0.5 --lr 1e-4 --critic_lr 5e-4 \
+    --ppo_epoch 15 --clip_param 0.2 --threshold ${threshold} --entropy_coef 0.05 \
     --cnn_layers_params "32,3,1,1 64,3,1,1 32,3,1,1" --use_eval --n_eval_rollout_threads 100 --save_interval 25 --log_inerval 10 \
     --overcooked_version ${version} \
     --wandb_name "zhiyuanli" --user_name "zhiyuanli" 
