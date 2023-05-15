5c7581bae2987e3622684647233caf9e7961a883
LiZhYun <18583960999@163.com>
2023-05-15 19:36:04 +0300

    matrix

--- bta/config.py
+++ bta/config.py
@@ -184,7 +184,7 @@ def get_config():
 
     # prepare parameters
     parser.add_argument("--algorithm_name", type=str,
-                        default='temporal', choices=["mappo", "gcs", "ar", "happo", "temporal"])
+                        default='temporal', choices=["mappo", "gcs", "ar", "happo", "hatrpo", "temporal"])
 
     parser.add_argument("--experiment_name", type=str, default="check", help="an identifier to distinguish different experiment.")
     parser.add_argument("--seed", type=int, default=1, help="Random seed for numpy/torch")
