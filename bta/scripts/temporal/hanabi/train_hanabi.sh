92c613c897da2a686ad5d5a05e6da5730fe80d93
LiZhYun <18583960999@163.com>
2023-05-15 17:02:06 +0300

    hanabi

--- bta/scripts/temporal/hanabi/train_hanabi.sh
+++ bta/scripts/temporal/hanabi/train_hanabi.sh
@@ -15,14 +15,14 @@ env="Hanabi"
 hanabi="Hanabi-Full"
 num_agents=2
 algo="temporal"
-exp="check"
+exp="Hanabi_2"
 deno=100
 threshold=`echo "scale=2; $SLURM_ARRAY_TASK_ID / $deno" | bc`
 
 python ../../train/train_hanabi.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
 --hanabi_name ${hanabi} --num_agents ${num_agents} --seed 1 --n_training_threads 1 --n_rollout_threads 1000 \
 --num_mini_batch 1 --episode_length 100 --num_env_steps 10000000000000 --ppo_epoch 15 --clip_param 0.2 \
---threshold ${threshold} --lr 7e-4 --critic_lr 1e-3 --gain 0.01 --hidden_size 512 --layer_N 2 --entropy_coef 0.015 --user_name "zhiyuanli" --wandb_name "zhiyuanli"
+--threshold ${threshold} --lr 7e-4 --critic_lr 1e-3 --gain 0.01 --hidden_size 512 --layer_N 2 --entropy_coef 0.05 --user_name "zhiyuanli" --wandb_name "zhiyuanli"
 
 # Deactivate the environment
 source deactivate
