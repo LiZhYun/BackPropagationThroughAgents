47999cda71fc08f139a7e9f3d364697041fadfa3
LiZhYun <18583960999@163.com>
2023-05-19 21:07:28 +0300

    sh

--- bta/scripts/temporal/football/triton/train_football_single_goal_versus_lazy.sh
+++ bta/scripts/temporal/football/triton/train_football_single_goal_versus_lazy.sh
@@ -4,14 +4,14 @@
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=32G
-#SBATCH --time=120:00:00
-#SBATCH --array=0-100:5
+#SBATCH --mem=64G
+#SBATCH --time=72:00:00
+#SBATCH --array=0-100
 
 # exp param
 env="Football"
 scenario="academy_single_goal_versus_lazy"
-algo="temporal" # "mappo" "ippo"
+algo="temporal"
 exp="check"
 
 # football param
@@ -28,6 +28,5 @@ CUDA_VISIBLE_DEVICES=0 singularity exec --bind /scratch --nv /scratch/work/liz23
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
---representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
---user_name "zhiyuanli" --wandb_name "zhiyuanli" \
---max_edges 10  --time_channels 100 --time_gap 10 --threshold ${threshold} --entropy_coef 0.025
+--representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --num_mini_batch 2 \
+--user_name "zhiyuanli" --wandb_name "zhiyuanli" --threshold ${threshold}
