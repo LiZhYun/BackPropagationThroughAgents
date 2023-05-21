8916693febe3b24bc737fa43d579b06d164dfe6c
Zhiyuan Li <lizhiyua@puhti-login14.bullx>
2023-05-21 22:44:13 +0300

    sh

--- bta/scripts/temporal/gobigger/triton/train_gobigger_t3p2.sh
+++ bta/scripts/temporal/gobigger/triton/train_gobigger_t3p2.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=gobigger-temporal
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=50
 #SBATCH --mem=256G
 #SBATCH --time=72:00:00
 #SBATCH --array=0-3
 
-module restore pytorch2002-di
+module restore bpta
 conda activate pytorch-env
 
 env="GoBigger"
@@ -23,14 +23,11 @@ threshold=1.0
 
 # train param
 num_env_steps=2500000
-episode_length=256
-
-echo "env is ${env}, senario is ${senario}, algo is ${algo}, exp is ${exp}"
+episode_length=1000
 
 python ../../../train/train_gobigger.py \
 --env_name ${env} --scenario_name ${scenario} --team_num ${team_num} --player_num_per_team ${player_num_per_team} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
---save_interval 100000 --log_interval 50000 --eval_interval 200000 --n_eval_rollout_threads 5 --eval_episodes 100 \
---representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 32 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 32 \
+--save_interval 100000 --log_interval 50000 --eval_interval 200000 --n_eval_rollout_threads 10 --eval_episodes 100 --n_rollout_threads 50 --num_mini_batch 1 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
---max_edges 10  --time_channels 100 --time_gap 10 --lr 1e-4 --critic_lr 1e-4 --threshold ${threshold} --max_grad_norm 0.5 --value_loss_coef 0.5
+--threshold ${threshold}
