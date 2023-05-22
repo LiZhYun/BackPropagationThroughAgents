56a19a986fc105257ae3f979c355f10af6f5b629
LiZhYun <18583960999@163.com>
2023-05-22 20:47:38 +0300

    sh

--- bta/scripts/temporal/hanabi/lumi/train_hanabi.sh
+++ bta/scripts/temporal/hanabi/lumi/train_hanabi.sh
@@ -42,8 +42,10 @@ case $SLURM_ARRAY_TASK_ID in
      ;;
 esac
 
+echo "env is ${env}, algo is ${algo}, exp is ${exp} threshold is ${threshold}"
+
 srun singularity exec -B"$SCRATCH:$SCRATCH" $SCRATCH/bpta_lumi.sif python ../../../train/train_hanabi.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
 --hanabi_name ${hanabi} --num_agents ${num_agents} --seed 1 --n_training_threads 1 --n_rollout_threads 1000 \
 --num_mini_batch 1 --episode_length 100 --num_env_steps 10000000000000 --ppo_epoch 15 \
---threshold ${threshold} --gain 0.01 --lr 7e-4 --critic_lr 1e-3 --hidden_size 512 --layer_N 2 --entropy_coef 0.015 \ 
+--threshold ${threshold} --gain 0.01 --lr 5e-5 --critic_lr 1e-3 --hidden_size 512 --layer_N 2 --entropy_coef 0.015 \ 
 --user_name "zhiyuanli" --wandb_name "zhiyuanli"
