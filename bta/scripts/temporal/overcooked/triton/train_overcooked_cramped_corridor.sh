552ba37f941365f40baf4b056dc8831865627819
LiZhYun <18583960999@163.com>
2023-05-22 19:48:21 +0300

    sh

--- bta/scripts/temporal/overcooked/triton/train_overcooked_cramped_corridor.sh
+++ bta/scripts/temporal/overcooked/triton/train_overcooked_cramped_corridor.sh
@@ -52,7 +52,7 @@ esac
 
 echo "env is ${env}, layout is ${layout}, algo is ${algo}, exp is ${exp} version is ${version}"
 
-ython ../../../train/train_overcooked.py \
+python ../../../train/train_overcooked.py \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --layout_name ${layout} --num_agents ${num_agents} \
     --seed 1 --n_training_threads 1 --n_rollout_threads 100 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --reward_shaping_horizon 100000000 \
     --ppo_epoch 15 --threshold ${threshold} \
