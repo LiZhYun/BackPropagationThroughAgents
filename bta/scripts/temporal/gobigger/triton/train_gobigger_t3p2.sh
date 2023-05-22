b78260e6280d06f233139a21dd420950e3b72ec5
LiZhYun <18583960999@163.com>
2023-05-22 14:59:32 +0300

    sh

--- bta/scripts/temporal/gobigger/triton/train_gobigger_t3p2.sh
+++ bta/scripts/temporal/gobigger/triton/train_gobigger_t3p2.sh
@@ -9,7 +9,7 @@
 #SBATCH --array=0-3
 
 module restore bpta
-conda activate pytorch-env
+source activate pytorch-env
 
 env="GoBigger"
 
@@ -23,11 +23,11 @@ threshold=1.0
 
 # train param
 num_env_steps=2500000
-episode_length=1000
+episode_length=200
 
 python ../../../train/train_gobigger.py \
 --env_name ${env} --scenario_name ${scenario} --team_num ${team_num} --player_num_per_team ${player_num_per_team} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
---save_interval 100000 --log_interval 50000 --eval_interval 200000 --n_eval_rollout_threads 10 --eval_episodes 100 --n_rollout_threads 50 --num_mini_batch 1 \
+--save_interval 100000 --log_interval 10000 --eval_interval 200000 --n_eval_rollout_threads 10 --eval_episodes 100 --n_rollout_threads 50 --num_mini_batch 1 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli" \
 --threshold ${threshold}
