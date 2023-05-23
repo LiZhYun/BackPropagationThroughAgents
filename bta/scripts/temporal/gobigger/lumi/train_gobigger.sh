2a46d8f742e94cb3c496349ceaadc1228dab58ea
LiZhYun <18583960999@163.com>
2023-05-23 18:09:03 +0300

    --max_grad_norm

--- bta/scripts/temporal/gobigger/lumi/train_gobigger.sh
+++ bta/scripts/temporal/gobigger/lumi/train_gobigger.sh
@@ -31,6 +31,6 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}"
 srun singularity exec -B $SCRATCH $SCRATCH/bpta_lumi.sif python ../../../train/train_gobigger.py \
 --env_name ${env} --team_num ${team_num} --player_num_per_team ${player_num_per_team} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
---save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 5 --eval_episodes 100 \
+--save_interval 200000 --log_interval 100000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 5 --eval_episodes 100 \
 --n_rollout_threads 50 --num_mini_batch 50 --max_grad_norm 0.5 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli"
