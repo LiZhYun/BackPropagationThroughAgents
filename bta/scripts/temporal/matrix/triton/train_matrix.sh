367d52c03fca55349614dced81fb860050531241
LiZhYun <18583960999@163.com>
2023-05-24 15:50:35 +0300

    sh

--- bta/scripts/temporal/matrix/triton/train_matrix.sh
+++ bta/scripts/temporal/matrix/triton/train_matrix.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name=matrix-temporal
+#SBATCH --output=./out/matrix-temporal_%A_%a.out # Name of stdout output file
+#SBATCH --error=./out/matrix-temporal_err_%A_%a.txt  # Name of stderr error file
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --mem=64G
+#SBATCH --time=72:00:00
 #SBATCH --array=0
 
 #--partition=gpu
@@ -28,15 +30,12 @@ num_agents=2
 # train param
 num_env_steps=25000000
 episode_length=200
-threshold=1.0
 
-echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, threshold is ${threshold}"
+echo "env is ${env}, scenario is ${scenario}, algo is ${algo}"
 
-python ../../train/train_matrix.py \
+python ../../../train/train_matrix.py \
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
---representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 100 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
---user_name "zhiyuanli" --wandb_name "zhiyuanli" \
---max_edges 10  --time_channels 100 --time_gap 10 --threshold ${threshold} --temperature 1.0
-# --max_grad_norm 3.5 --lr 5e-4 --critic_lr 5e-4 --entropy_coef 0.05 \
+--n_rollout_threads 100 --ppo_epoch 15 --clip_param 0.2 --num_mini_batch 2 \
+--user_name "zhiyuanli" --wandb_name "zhiyuanli"
