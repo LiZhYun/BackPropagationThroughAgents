fix other agents' gradient bug in baserunner

bta/runner/temporal/base_runner.py
@@ -200,22 +200,19 @@ class Runner(object):
         for idx, agent_id in enumerate(reversed(ordered_vertices)):
             self.trainer[agent_id].prep_training()
             self.buffer[agent_id].update_factor(factor)
-            numerator = np.zeros((self.episode_length, self.n_rollout_threads, action_dim), dtype=np.float32)
-            denominator = np.ones((self.episode_length, self.n_rollout_threads, action_dim), dtype=np.float32)
+
+            # other agents' gradient to agent_id
+            action_grad_per_agent = np.zeros((self.episode_length, self.n_rollout_threads, action_dim), dtype=np.float32)
             for updated_agent in reversed(ordered_vertices)[0:idx]:
-                multiplier = np.concatenate([new_actions_probs[agent_id+1:updated_agent], new_actions_probs[updated_agent+1:]],0)
-                multiplier = np.ones((self.episode_length, self.n_rollout_threads, 1), dtype=np.float32) if multiplier is None else np.prod(multiplier, 0)
-                numerator += action_grad[updated_agent][agent_id] * multiplier
-                denominator *= old_actions_probs[updated_agent]
-            action_grad_per_agent = numerator / denominator
-            # action_grad_per_agent = np.zeros((self.episode_length, self.n_rollout_threads, action_dim), dtype=np.float32)
-            # for updated_agent in reversed(ordered_vertices)[0:idx]:
-            #     action_grad_per_agent += action_grad[updated_agent][agent_id]
+                numerator = np.concatenate([new_actions_probs[agent_id+1:updated_agent], new_actions_probs[updated_agent+1:]],0)
+                denominator = np.concatenate([old_actions_probs[agent_id+1:updated_agent], old_actions_probs[updated_agent+1:]],0)
+                numerator = np.ones((self.episode_length, self.n_rollout_threads, 1), dtype=np.float32) if numerator is None else np.prod(numerator, 0)
+                denominator = np.ones((self.episode_length, self.n_rollout_threads, 1), dtype=np.float32) if denominator is None else np.prod(denominator, 0)
+                action_grad_per_agent += action_grad[updated_agent][agent_id] * (numerator / denominator)
             self.buffer[agent_id].update_action_grad(action_grad_per_agent)
             available_actions = None if self.buffer[agent_id].available_actions is None \
                 else self.buffer[agent_id].available_actions[:-1].reshape(-1, *self.buffer[agent_id].available_actions.shape[2:])
             
-            # tmp_agent_order = self.agent_order[0].clone()
             tmp_agent_order = ordered_vertices.clone()
             agent_order = torch.stack([tmp_agent_order for _ in range(self.episode_length*self.n_rollout_threads)]).to(self.device)
             # agent_order = torch.stack([torch.randperm(self.num_agents) for _ in range(self.episode_length*self.n_rollout_threads)]).to(self.device)
@@ -298,7 +295,7 @@ class Runner(object):
             new_actions_probs[agent_id] = _t2n(torch.exp(new_actions_logprob)).reshape(self.episode_length,self.n_rollout_threads,1)
 
             self.trainer[agent_id].policy.actor_optimizer.zero_grad()
-            torch.sum(torch.prod(torch.clamp(torch.exp(new_actions_logprob), 1.0 - self.inner_clip_param, 1.0 + self.inner_clip_param),dim=-1), dim=-1).mean().backward()
+            torch.sum(torch.prod(torch.clamp(torch.exp(new_actions_logprob-old_actions_logprob.detach()), 1.0 - self.inner_clip_param, 1.0 + self.inner_clip_param),dim=-1), dim=-1).mean().backward()
             for i in range(self.num_agents):
                 action_grad[agent_id][i] = _t2n(one_hot_actions.grad[:,i]).reshape(self.episode_length,self.n_rollout_threads,action_dim)
             factor = factor*_t2n(torch.prod(torch.clamp(torch.exp(new_actions_logprob-old_actions_logprob), 1.0 - self.inner_clip_param, 1.0 + self.inner_clip_param),dim=-1).reshape(self.episode_length,self.n_rollout_threads,1))