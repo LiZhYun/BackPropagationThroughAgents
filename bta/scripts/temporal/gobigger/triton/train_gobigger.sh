6dd4a3ab209c6114afe7ce81ba333112e74bd045
LiZhYun <18583960999@163.com>
2023-06-20 14:38:11 +0300

    triton train

--- bta/scripts/temporal/gobigger/triton/train_gobigger.sh
+++ bta/scripts/temporal/gobigger/triton/train_gobigger.sh
@@ -3,11 +3,12 @@
 #SBATCH --job-name=gobigger-temporal
 #SBATCH --output=./out/gobigger-temporal_%A_%a.out # Name of stdout output file
 #SBATCH --error=./out/gobigger-temporal_err_%A_%a.txt  # Name of stderr error file
-#SBATCH --partition=batch
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=50
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=256G
-#SBATCH --time=72:00:00
+#SBATCH --time=120:00:00
 #SBATCH --array=0-4
 
 module restore bpta
@@ -32,6 +33,6 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}"
 python ../../../train/train_gobigger.py \
 --env_name ${env} --team_num ${team_num} --player_num_per_team ${player_num_per_team} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
---save_interval 200000 --log_interval 10000 --eval_interval 400000 --n_eval_rollout_threads 5 --eval_episodes 100 \
---n_rollout_threads 5 --num_mini_batch 50 --max_grad_norm 10.0 \
+--save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 10 --eval_episodes 100 \
+--n_rollout_threads 20 --num_mini_batch 1 \
 --user_name "zhiyuanli" --wandb_name "zhiyuanli"
