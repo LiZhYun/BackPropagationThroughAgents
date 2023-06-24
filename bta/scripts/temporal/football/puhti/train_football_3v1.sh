de7117c9c13976d935b15b5e82ad58ae1a2a9ea8
Zhiyuan Li <lizhiyua@puhti-login15.bullx>
2023-06-25 01:30:05 +0300

    script

--- bta/scripts/temporal/football/puhti/train_football_3v1.sh
+++ bta/scripts/temporal/football/puhti/train_football_3v1.sh
@@ -4,10 +4,11 @@
 #SBATCH --output=./out/football-3v1-temporal_%A_%a.out # Name of stdout output file
 #SBATCH --error=./out/football-3v1-temporal_err_%A_%a.txt  # Name of stderr error file
 #SBATCH --account=project_2007776
-#SBATCH --partition=small
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=256G
 #SBATCH --time=72:00:00
 #SBATCH --array=0-4
 
@@ -33,5 +34,5 @@ apptainer_wrapper exec python ../../../train/train_football.py \
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
---representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --num_mini_batch 2 --max_grad_norm 10.0 \
---user_name "zhiyuanli" --wandb_name "zhiyuanli"
+--representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --num_mini_batch 2 \
+--user_name "zhiyuanli" --wandb_name "zhiyuanli" --skip_connect
