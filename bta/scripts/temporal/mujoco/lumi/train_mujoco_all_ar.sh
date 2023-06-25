1249144b8b30a0bbdc91c85566e627e70c3ca534
LiZhYun <18583960999@163.com>
2023-06-25 23:01:04 +0300

    mujoco

---
+++ bta/scripts/temporal/mujoco/lumi/train_mujoco_all_ar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# cramped_corridor diff_orders h_far_tomato multiplayer_schelling(old) soup_coordination
+sbatch train_mujoco_ar.sh HalfCheetah-v2 2x3
+sbatch train_mujoco_ar.sh HalfCheetah-v2 3x2
+sbatch train_mujoco_ar.sh HalfCheetah-v2 6x1
+sbatch train_mujoco_ar.sh Ant-v2 2x4
+sbatch train_mujoco_ar.sh Ant-v2 4x2
+sbatch train_mujoco_ar.sh Ant-v2 8x1
+sbatch train_mujoco_ar.sh Walker2d-v2 2x3
+sbatch train_mujoco_ar.sh Walker2d-v2 3x2
+sbatch train_mujoco_ar.sh Walker2d-v2 6x1
