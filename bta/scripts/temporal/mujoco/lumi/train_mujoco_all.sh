0528b4c9e26e7249eadc77748c256d360b000cc2
LiZhYun <18583960999@163.com>
2023-07-09 15:48:14 +0300

    runner

--- bta/scripts/temporal/mujoco/lumi/train_mujoco_all.sh
+++ bta/scripts/temporal/mujoco/lumi/train_mujoco_all.sh
@@ -7,7 +7,10 @@ sbatch train_mujoco.sh HalfCheetah-v2 6x1
 sbatch train_mujoco.sh Ant-v2 2x4
 sbatch train_mujoco.sh Ant-v2 4x2
 sbatch train_mujoco.sh Ant-v2 8x1
+sbatch train_mujoco.sh Ant-v2 2x4d
 sbatch train_mujoco.sh Walker2d-v2 2x3
 sbatch train_mujoco.sh Walker2d-v2 3x2
 sbatch train_mujoco.sh Walker2d-v2 6x1
 sbatch train_mujoco.sh Humanoid-v2 17x1
+sbatch train_mujoco.sh Hopper-v2 3x1
+sbatch train_mujoco.sh Reacher-v2 2x1
