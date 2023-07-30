f588f1cf0b1ed1dfa28ee63d5dc9893d3077c478
LiZhYun <18583960999@163.com>
2023-07-30 21:51:42 +0300

    gcs

--- bta/runner/temporal/smac_runner.py
+++ bta/runner/temporal/smac_runner.py
@@ -21,6 +21,7 @@ from icecream import ic
 from scipy.stats import rankdata
 import igraph as ig
 from bta.algorithms.utils.util import check
+from bta.algorithms.utils.distributions import FixedCategorical, FixedNormal
 from functools import reduce
 
 def _t2n(x):
@@ -50,22 +51,23 @@ class SMACRunner(Runner):
             for step in range(self.episode_length):
                 # Sample actions
                 values, actions, hard_actions, action_log_probs, rnn_states, \
-                    rnn_states_critic, joint_actions, joint_action_log_probs = self.collect(step)
+                    rnn_states_critic, joint_actions, joint_action_log_probs, joint_values = self.collect(step)
                     
                 # Obser reward and next obs
                 env_actions = joint_actions if joint_actions is not None else hard_actions
                 obs, share_obs, rewards, dones, infos, available_actions = self.envs.step(env_actions)
                 data = obs, share_obs, rewards, dones, infos, available_actions, values, actions, hard_actions, action_log_probs, \
-                    rnn_states, rnn_states_critic, joint_actions, joint_action_log_probs
+                    rnn_states, rnn_states_critic, joint_actions, joint_action_log_probs, joint_values
                 
                 # insert data into buffer
                 self.insert(data)
 
             # compute return and update network
             self.compute()
-            # if self.use_action_attention:
-            self.joint_compute()
-            train_infos = self.joint_train() if self.use_action_attention else self.train()
+            if self.use_action_attention:
+                self.joint_compute()
+
+            train_infos = self.joint_train() if self.use_action_attention else [self.train_seq_agent, self.train_seq_epoch, self.train_sim][self.train_sim_seq]()
             
             # post process
             total_num_steps = (episode + 1) * self.episode_length * self.n_rollout_threads
@@ -181,12 +183,21 @@ class SMACRunner(Runner):
             rnn_states[:, agent_idx] = _t2n(rnn_state)
             rnn_states_critic[:, agent_idx] = _t2n(rnn_state_critic)
 
-        joint_actions, joint_action_log_probs = None, None
+        joint_actions, joint_action_log_probs, joint_values = None, None, None
         if self.use_action_attention:
             available_actions_all = np.stack([self.buffer[agent_idx].available_actions[step] for agent_idx in range(self.num_agents)],1)
-            joint_actions, joint_action_log_probs = self.action_attention(logits, obs_feats, available_actions=available_actions_all, tau=self.temperature)
-            joint_actions = _t2n(torch.argmax(joint_actions, -1, keepdim=True).to(torch.int))
+            bias_, joint_values = self.action_attention.get_actions(logits, obs_feats, available_actions=available_actions_all, tau=self.temperature)
+            if self.discrete:
+                joint_dist = FixedCategorical(logits=logits+bias_)
+            else:
+                action_mean = logits+bias_
+                action_std = torch.sigmoid(self.log_std / self.std_x_coef) * self.std_y_coef
+                joint_dist = FixedNormal(action_mean, action_std)
+            joint_actions = joint_dist.sample()
+            joint_action_log_probs = joint_dist.log_probs_joint(joint_actions)
+            joint_actions = _t2n(joint_actions)
             joint_action_log_probs = _t2n(joint_action_log_probs)
+            joint_values = _t2n(joint_values)
             for agent_idx in range(self.num_agents):
                 ego_exclusive_action = actions[:,0:self.num_agents]
                 tmp_execution_mask = torch.stack([torch.zeros(self.n_rollout_threads)] * self.num_agents, -1).to(self.device)
@@ -202,7 +213,7 @@ class SMACRunner(Runner):
                 )
                 action_log_probs[:, agent_idx] = _t2n(action_log_prob)
 
-        return values, actions, hard_actions, action_log_probs, rnn_states, rnn_states_critic, joint_actions, joint_action_log_probs
+        return values, actions, hard_actions, action_log_probs, rnn_states, rnn_states_critic, joint_actions, joint_action_log_probs, joint_values
 
     def collect_eval(self, step, eval_obs, eval_rnn_states, eval_masks, available_actions):
         actions = np.zeros((self.n_eval_rollout_threads, self.num_agents, self.action_dim))
@@ -242,7 +253,7 @@ class SMACRunner(Runner):
 
     def insert(self, data):
         obs, share_obs, rewards, dones, infos, available_actions, values, actions, hard_actions, action_log_probs, \
-            rnn_states, rnn_states_critic, joint_actions, joint_action_log_probs = data
+            rnn_states, rnn_states_critic, joint_actions, joint_action_log_probs, joint_values = data
         
         dones_env = np.all(dones, axis=-1)
 
@@ -276,7 +287,8 @@ class SMACRunner(Runner):
                                         active_masks=active_masks[:, agent_id],
                                         available_actions=available_actions[:, agent_id],
                                         joint_actions=joint_actions,
-                                        joint_action_log_probs=joint_action_log_probs
+                                        joint_action_log_probs=joint_action_log_probs,
+                                        joint_value_preds=joint_values
                                         )
 
     @torch.no_grad()
