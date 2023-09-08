d4e17da885fa61cc46271af3b1d6a3a4f9952ae5
LiZhYun <18583960999@163.com>
2023-09-08 12:11:15 +0300

    token 4

--- bta/scripts/temporal/matrix/lumi/train_matrix_c.sh
+++ bta/scripts/temporal/matrix/lumi/train_matrix_c.sh
@@ -6,9 +6,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=10G
-#SBATCH --time=01:00:00
-#SBATCH --partition=small-g
-#SBATCH --gpus-per-node=1
+#SBATCH --time=02:00:00
+#SBATCH --partition=small
 #SBATCH --account=project_462000277
 #SBATCH --array=0-4
 
@@ -23,6 +22,7 @@ exp="check"
 num_env_steps=1000000
 episode_length=200
 # num_agents=50
+mix_std_y_coef=$1
 # layer=$1
 # token_factor=$2
 # channel_factor=$3
@@ -32,5 +32,6 @@ srun singularity exec -B"$SCRATCH:$SCRATCH" $SCRATCH/bpta_lumi.sif python ../../
 --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --save_interval 100000 --log_interval 10000 --use_eval --eval_interval 10000 --n_eval_rollout_threads 100 --eval_episodes 100 \
 --n_rollout_threads 50 --num_mini_batch 1 \
---user_name "zhiyuanli" --wandb_name "zhiyuanli"
+--user_name "zhiyuanli" --wandb_name "zhiyuanli" \
+--use_action_attention --mix_std_y_coef ${mix_std_y_coef}
 # --use_action_attention --attn_N ${layer} --token_factor ${token_factor} --channel_factor ${channel_factor}
